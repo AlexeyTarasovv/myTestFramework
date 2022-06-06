22575828a22b44828cea6289f5203f76a2fa6232
AlexeyTarasovv <coolerr195@gmail.com>
2022-06-06 16:56:39 +0300

    add after,before methods, add homePageTests class

---
+++ src/main/java/com/test/constants/FrameworkConstants.java
@@ -0,0 +1,12 @@
+package com.test.constants;
+
+public final class FrameworkConstants {
+
+    private FrameworkConstants() {}
+
+    private static final String CHROMEDRIVERPATH = System.getProperty("user.dir") + "/src/test/resources/executables/chromedriver";
+
+    public static String getChromedriverpath() {
+        return CHROMEDRIVERPATH;
+    }
+}
