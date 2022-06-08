03f4f03682d95a10a9afa46a4816af3df069c03d
AlexeyTarasovv <coolerr195@gmail.com>
2022-06-08 11:29:05 +0300

    refactor, add exception in case of no property value in property file

--- src/main/java/com/test/utils/ReadPropertyFile.java
+++ src/main/java/com/test/utils/ReadPropertyFile.java
@@ -3,6 +3,7 @@ package com.test.utils;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Properties;
 
 public class ReadPropertyFile {
@@ -10,10 +11,15 @@ public class ReadPropertyFile {
     private ReadPropertyFile() {}
 
     public static String get(String key) throws IOException {
+        String value = "";
+
         Properties property = new Properties();
         FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/config/config.properties");
 
         property.load(file);
-        return property.getProperty(key);
+        value = property.getProperty(key);
+        //Exception in case of no property value in property file
+        if (Objects.isNull(value)) throw new NullPointerException("Property name " + key + " is not found. Please check config.properties");
+        return value;
     }
 }
