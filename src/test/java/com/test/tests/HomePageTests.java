e4bfffcbce6f7eb38bc4ce269f9bc408ef570cff
AlexeyTarasovv <coolerr195@gmail.com>
2022-06-09 17:08:57 +0300

    rewrite test_case using AssertJ

--- src/test/java/com/test/tests/HomePageTests.java
+++ src/test/java/com/test/tests/HomePageTests.java
@@ -1,12 +1,14 @@
 package com.test.tests;
 
 import com.test.driver.DriverManager;
+import org.assertj.core.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import java.util.List;
 import java.util.Objects;
 
@@ -21,22 +23,18 @@ public class HomePageTests extends BaseTest{
         driver.findElement(By.xpath("//input[@id='searchInput']")).sendKeys("QA Testing", Keys.ENTER);
 
         //QA Testing - Search results - Wikipedia
-
         String title = driver.getTitle();
-        Assert.assertTrue(Objects.nonNull(title), "Title is null");
-        Assert.assertTrue(title.toLowerCase().contains("wikipedia"));
-        Assert.assertTrue(title.length() > 15 && title.length() < 100);
+        Assertions.assertThat(title)
+                .as("Object is actually null").isNotNull()
+                .containsIgnoringCase("wikipedia")
+                .as("Doesn't have size between 15 and 100").hasSizeBetween(15, 100);
+
 
         List<WebElement> links = driver.findElements(By.xpath("//ul[@class='mw-search-results']/li/div/a"));
-        Assert.assertEquals(links.size(), 20);
 
-        boolean isElementPresent = false;
-        for (WebElement e : links) {
-            if (e.getText().equalsIgnoreCase("game testing")){
-                isElementPresent = true;
-                break;
-            }
-        }
-        Assert.assertTrue(isElementPresent, "Expected link not found");
+        Assertions.assertThat(links)
+                .hasSize(20)
+                .extracting(e -> e.getText().toLowerCase())
+                .contains("game testing");
     }
 }
