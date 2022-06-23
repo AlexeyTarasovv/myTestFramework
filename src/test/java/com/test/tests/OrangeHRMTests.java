2fecf3340ac4138dc22c27bbcbfd4768943e366e
AlexeyTarasovv <coolerr195@gmail.com>
2022-06-23 17:21:17 +0300

    add standalone test with dataProvider, refactor

--- src/test/java/com/test/tests/OrangeHRMTests.java
+++ src/test/java/com/test/tests/OrangeHRMTests.java
@@ -2,30 +2,21 @@ package com.test.tests;
 
 import com.test.pages.OrangeHRMLoginPage;
 import org.assertj.core.api.Assertions;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public final class OrangeHRMTests extends BaseTest{
 
     private OrangeHRMTests(){}
 
-    @Test(dataProvider = "LoginTestDataProvider")
-    public void loginLogoutTest(String username, String password) throws InterruptedException {
+    @Test
+    public void loginLogoutTest() {
 
         String title = new OrangeHRMLoginPage()
-                .enterUserName(username).enterPassword(password).clickLogin()
+                .enterUserName("Admin").enterPassword("admin123").clickLogin()
                 .clickWelcome().clickLogout()
                 .getTitle();
 
         Assertions.assertThat(title)
                 .isEqualTo("OrangeHRM");
-    }
-
-    @DataProvider(name="LoginTestDataProvider", parallel = true)
-    public Object[][] getData(){
-        return new Object[][] {
-                {"Admin", "admin123"},
-                {"Admin11111", "addddddd"}
-        };
     }
 }
---
+++ src/test/java/com/test/tests/OrangeHRMWithDataProviderTests.java
@@ -0,0 +1,31 @@
+package com.test.tests;
+
+import com.test.pages.OrangeHRMLoginPage;
+import org.assertj.core.api.Assertions;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public final class OrangeHRMWithDataProviderTests extends BaseTest{
+
+    private OrangeHRMWithDataProviderTests(){}
+
+    @Test(dataProvider = "LoginTestDataProvider")
+    public void loginLogoutTest(String username, String password) {
+
+        String title = new OrangeHRMLoginPage()
+                .enterUserName(username).enterPassword(password).clickLogin()
+                .clickWelcome().clickLogout()
+                .getTitle();
+
+        Assertions.assertThat(title)
+                .isEqualTo("OrangeHRM");
+    }
+
+    @DataProvider(name="LoginTestDataProvider", parallel = true)
+    public Object[][] getData(){
+        return new Object[][] {
+                {"Admin", "admin123"},
+                {"Admin11111", "addddddd"}
+        };
+    }
+}
